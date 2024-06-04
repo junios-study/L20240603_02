1583fd2608f2029e876a43fd85c5abd47846a094
junios <junios@naver.com>
2024-06-04 13:22:42 +0900

    정렬 함수 사용 void* 함수 포인터

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 #include "Engine.h"
-
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main2()
+int compare(const void* PA, const void* PB)
+{
+	int A = *(int*)PA;
+	int B = *(int*)PB;
+
+	return A - B;
+
+	//if (A > B)
+	//{
+	//	return 1;
+	//}
+	//else if (A < B)
+	//{
+	//	return -1;
+	//}
+	//else
+	//{
+	//	return 0;
+	//}
+
+
+}
+
+int main()
 {
 	int Numbers[5] = { 5, 4, 2, 1, 3 };
-	
+
+	qsort(Numbers, 5, sizeof(int), compare);
+
 	for (auto N : Numbers)
 	{
 		cout << N << ", ";
@@ -18,7 +43,7 @@ int main2()
 }
 
 
-int main()
+int main2()
 {
 	UEngine* MyEngine = new UEngine();
 
