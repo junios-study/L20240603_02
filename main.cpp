ea324c9e1f46934392a8ef361b9f2bd38cbc767a
junios <junios@naver.com>
2024-06-04 15:32:24 +0900

    함수 포인터 void* 형태

--- main.cpp
+++ main.cpp
@@ -1,106 +1,68 @@
 #include "Engine.h"
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int Compare(int A, int B)
+void T()
 {
-	return A - B;
+	cout << "T" << endl;
 }
 
-void Sort(void* PA, int Size, int (*CompareFunction)(int A, int B))
+void Y()
 {
-	int* IntPA = (int*)PA;
+	cout << "Y" << endl;
+}
 
-	for (int i = 0; i < Size; ++i)
+void Test(void (*FunctionPointer)())
+{
+	for (int i = 0; i < 100; ++i)
 	{
-		for (int j = 0; j < Size; ++j)
+		if (i > 50)
 		{
-			if (CompareFunction(IntPA[i], IntPA[j]) > 0 )
-			{
-				int Temp = IntPA[i];
-				IntPA[i] = IntPA[j];
-				IntPA[j] = Temp;
-			}
+			FunctionPointer();
 		}
 	}
-
-	for (int i = 0; i < Size; ++i)
-	{
-		cout << IntPA[i] << ", ";
-	}
 }
 
-
-void (*fp)(void* PA, int Size, int ElementOfSize);
-
-void (*fp2)();
-
-void Test()
+void EnterPointer(void* P,int Count, int ElementOfSize)
 {
-	cout << "Test" << endl;
+
 }
 
-void Test2()
+class UObject
 {
-	cout << "Test2" << endl;
-}
 
-void (*FunctionPointer)(int A, int B);
+};
 
-void Add(int A, int B)
+class AActor : public UObject
 {
-	cout << A + B << endl;
-}
 
-void Sub(int A, int B)
-{
-	cout << A - B << endl;
-}
+};
 
-void Operator(int A, int B, void (*FunctionPointer)(int A, int B))
+class APawn : public AActor
 {
-	cout << FunctionPointer << endl;
-}
 
-int Compare2(int A, int B)
-{
-	return B - A;
-}
+};
 
 int main()
 {
-	int Numbers2[4] = { 5, 4, 2, 1 };
-	//Sort(Numbers2, 4, Compare2);
+	UObject* Object = new UObject();
+	UObject* Object = new AActor();
+	UObject* Object = new APawn();
+
+	int* PA = nullptr;
+	float* PF = nullptr;
+
+	EnterPointer(PA);
+	EnterPointer(PF);
 
 
-	//int* PA = Numbers;
-	//float Numbers[4] = { 5.3f, 4.1f, 2.0f, 1.67f };
-	//int* PA2 = Numbers2;
-	//float* PA = Numbers;
-
-
-	Operator(1, 2, Add);
-	//Operator(1, 2, Sub);
-
-	//fp2 = Test2;
-
-	//cout << fp2 << endl;
-	//cout << Test << endl;
-
-	//Test();
-	//fp2();
-
-	//cout << *(PA2+0) << endl;
-	//cout << NPA2[0] << endl;
-
-
-	//Sort(PA2, sizeof(Numbers) / sizeof(float), sizeof(float));
-
-
+	
 	return 0;
 }
+
 
 
 int main2()
