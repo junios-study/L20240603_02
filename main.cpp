cbb175682498fed63c2adc3b954211b3709b6ee9
junios <junios@naver.com>
2024-06-10 12:53:07 +0900

    싱글톤 연습

--- main.cpp
+++ main.cpp
@@ -3,7 +3,50 @@
 
 using namespace std;
 
+class Singleton
+{
+public:
+
+private:
+	Singleton();
+	int Number;
+	static Singleton* Instanace;
+
+public:
+	static void Add(Singleton B)
+	{
+		
+	}
+
+	static Singleton* GetInstance()
+	{
+		if (!Instanace)
+		{
+			Instanace = new Singleton();
+		}
+
+		return Instanace;
+	}
+};
+
+Singleton* Singleton::Instanace = nullptr;
+
+#define GSinglton Singleton::GetInstance()
+
+
 int main()
+{
+	GSinglton;
+	GSinglton;
+	GSinglton;
+	GSinglton;
+	GSinglton;
+
+	return 0;
+}
+
+
+int main2()
 {
 	GEngine->LoadLevel("data.map");
 
