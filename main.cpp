c56401fe0562b3980065c2c7dca4bff7bd4aea2a
junios <junios@naver.com>
2024-06-11 16:00:16 +0900

    string class 예제

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS
+
 #include "Engine.h"
+#include <string>
+
+//#include <string.h>
 
 int SDL_main(int argc, char* argv[])
 {
+	std::string name = "junios";
+	std::string name2;
+
+	name2 = name;
+
+	char Data[100];
+	char Data2[100];
+
+	strcpy(Data, "Junios");
+	for (int i = 0; i < 100; ++i)
+	{
+		if (Data[i] == '\0')
+		{
+			Data2[i] = '\0';
+			break;
+		}
+		Data2[i] = Data[i];
+	}
+	
+
 	GEngine->LoadLevel("data.map");
 	GEngine->Run();
 
